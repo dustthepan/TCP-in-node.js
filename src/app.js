c5d05150ed7dfcab4e7cb4c51f15cec77862cc49
Dustin Tran <dustthepan@gmail.com>
2020-02-03 01:28:21 -0800

    create open connection

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 const net = require('net');
-const rl = require('readline');
+const rl = require('readline-sync');
+
+const host = '127.0.0.1'
+const port ='9000'
+
+let app = null;
+
+const initiateConnection 
+
+const inputInteger = async () => {
+
+   let input = rl.question('Please Enter Integer',(number) =>{
+
+   })
+}
